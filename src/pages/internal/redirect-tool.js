3009d2770d12064d71cfdd4e98ece2aaa8c9c75b
Adam Bohannon <abo46n2@gmail.com>
2021-07-01 15:51:22 -0700

    Added form

--- src/pages/internal/redirect-tool.js
+++ src/pages/internal/redirect-tool.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Box } from '@material-ui/core'
 
 const RedirectTool = props => {
   return (
-    <div>
-      Redirect tool
-    </div>
+    <Box width={600}>
+      <form name="redirect" netlify>
+        <label>Long URL</label>
+        <input type="text" name="longUrl" />
+        <button type="submit">Submit</button>
+      </form>
+    </Box>
+
+
   )
 }
 
