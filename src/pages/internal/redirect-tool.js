c3a95c056c40e0f2f3f3b191c891b361714b30a0
Adam Bohannon <abo46n2@gmail.com>
2021-07-01 15:10:08 -0700

    wip

---
+++ src/pages/internal/redirect-tool.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RedirectTool = props => {
+  return (
+    <div>
+      Redirect tool
+    </div>
+  )
+}
+
+RedirectTool.propTypes = {
+
+}
+
+export default RedirectTool
