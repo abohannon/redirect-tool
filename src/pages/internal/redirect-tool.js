4e1deeec9ca67b42aa04f767f900ce80e3c88aee
Adam Bohannon <abo46n2@gmail.com>
2021-07-01 16:02:25 -0700

    wip form

--- src/pages/internal/redirect-tool.js
+++ src/pages/internal/redirect-tool.js
@@ -5,10 +5,25 @@ import { Box } from '@material-ui/core'
 const RedirectTool = props => {
   return (
     <Box width={600}>
-      <form name="redirect" method="POST" netlify>
-        <label>Long URL <input type="text" name="longUrl" /></label>
-
-        <button type="submit">Submit</button>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
     </Box>
 
