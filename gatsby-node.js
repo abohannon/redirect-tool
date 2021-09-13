6828ae8c54914d91a4e5f55d392d9cb13dd23640
Adam Bohannon <abo46n2@gmail.com>
2021-09-13 13:17:44 -0700

    Added HCP link

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,17 @@
 const fs = require("fs")
 const fetch = require("node-fetch")
 
-// exports.onPostBuild = () => {
-//   const buildDir = "public"
+exports.onPostBuild = () => {
+  const buildDir = "public"
 
-//   const stream = fs.createWriteStream(buildDir + "/_redirects", { flags: "a" })
+  const stream = fs.createWriteStream(buildDir + "/_redirects", { flags: "a" })
+  const routes = [
+    "/hello-friend/ /hello-goodbye/ 301!",
+    "/a/ /b/?utm_source=foo 301!",
+    "/yes/ /no/?utm_campaign=bar 301!",
+  ]
 
-//   routes.forEach(route => {
-//     stream.write(route + "\n")
-//   })
-// }
+  routes.forEach(route => {
+    stream.write(route + "\n")
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,9 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
+      <a href="https://www.housecallpro-staging.com">
+        Go to Housecall Pro staging
+      </a>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
