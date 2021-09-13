5ae06bd61d91c445cbfde714028f2b5dbfada070
Adam Bohannon <abo46n2@gmail.com>
2021-09-13 13:21:55 -0700

    Added p tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,8 @@ const IndexPage = () => (
       <a href="https://www.housecallpro-staging.com">
         Go to Housecall Pro staging
       </a>
+    </p>
+    <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
